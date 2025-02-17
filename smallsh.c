d15b25e8c2a3e63dcbfee67a13e362456f3c360d
Justin Rous <rousj@oregonstate.edu>
2025-02-17 06:27:54 +0000

    Implemented exit command structure. Non built-in command function in progress

--- smallsh.c
+++ smallsh.c
@@ -2,11 +2,43 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // Define Constants
 #define INPUT_LENGTH 2048
 #define MAX_ARGS 512
 const char* comment = "#";
+
+void command(void) {
+	/*
+	** Runs a command from the shell
+	**
+	** Code adapted from Canvas Exploration: Process API - Creating and Terminating Processes
+	** Date: 02/16/2025
+	** URL: https://canvas.oregonstate.edu/courses/1987883/pages/exploration-process-api-creating-and-terminating-processes?module_item_id=24956218
+	*/
+	pid_t spawnpid = -5;
+	spawnpid = fork();
+
+	switch(spawnpid) {
+		case -1:
+			// Error 
+			perror("fork() failed");
+			exit(EXIT_FAILURE);
+			break;
+		case 0:
+			// Child process
+			break;
+		default:
+			// Parent process
+
+			// Wait for spawnid to terminate
+			break;
+	}
+
+
+}
 
 struct command_line
 {
@@ -16,6 +48,7 @@ struct command_line
 	char *output_file;
 	bool is_bg;
 	bool is_empty;
+	bool exit;
 };
 
 struct command_line *parse_input()
@@ -36,19 +69,19 @@ struct command_line *parse_input()
 	// Tokenize the input
 	char *token = strtok(input, " \n");
 
+	// Check input for comment or blank line
 	if (input[0] == '\n') {
 		curr_command->is_empty = true;
 		return curr_command;
 	}
-
-	if (!strcmp(token, comment)) {
+	else if (!strcmp(token, comment)) {
 		curr_command->is_empty = true;
 		return curr_command;
 	}
-	
 	else {
 		curr_command->is_empty=false;
 	}
+
 	while(token){
 		if(!strcmp(token,"<")){
 			curr_command->input_file = strdup(strtok(NULL," \n"));
@@ -61,6 +94,26 @@ struct command_line *parse_input()
 		}
 		token=strtok(NULL," \n");
 	}
+	
+
+	// Check for Shell Built-in Commands
+	if (!strcmp(curr_command->argv[0], "exit")) {
+		curr_command->exit = true;
+		// Kill all processes/jobs that have been created by the shell
+	}
+	else if (!strcmp(curr_command->argv[0], "cd") {
+		// Implement cd func
+	}
+
+	else if (!strcmp(curr_command->argv[0], "status") {
+		// Implement status func
+	}
+	else {
+		// Run built in command 
+		command();
+
+	}
+
 	return curr_command;
 }
 
@@ -75,14 +128,11 @@ int main()
 			free(curr_command);
 			continue;
 		}
+		else if (curr_command->exit) {
+			return EXIT_SUCCESS;
+		}
 		else {
-			int count = 0;
-			char* curr_input = curr_command->argv[count];
-			while(curr_input != NULL)  {
-				printf("Curr input: %s\n", curr_input);
-				count++;
-				curr_input = curr_command->argv[count];
-			}	
+			printf("Finsihed");
 		}
 	}
 	return EXIT_SUCCESS;
